e5ae0aa8c94f3bcdf9b2e705390326cd621f27db
KungaCoding <95586873+KungaCoding@users.noreply.github.com>
2023-04-15 20:24:25 -0400

    Initial commit

---
+++ network/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    following = models.ManyToManyField("self", symmetrical=False, related_name="followers", blank=True)
+    
+    def __str__(self):
+        return self.username
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.user.username}'s post at {self.timestamp}"
---
+++ network/views.py
@@ -0,0 +1,105 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+from .models import User, Post
+from .forms import PostForm
+
+
+@login_required
+def index(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect('index')
+    else:
+        form = PostForm()
+    posts = Post.objects.all().order_by('-timestamp')
+    context = {'form': form, 'posts': posts}
+    return render(request, "network/index.html", context)
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "network/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "network/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "network/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "network/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "network/register.html")
+
+
+def profile(request, username):
+    user_profile = get_object_or_404(User, username=username)
+    posts = Post.objects.filter(user=user_profile).order_by('-timestamp')
+
+    context = {
+        'user_profile': user_profile,
+        'posts': posts,
+    }
+
+    if request.method == "POST":
+        action = request.POST["action"]
+        if request.user != user_profile:
+            if action == "Follow":
+                request.user.following.add(user_profile)
+                context["action"] = "Unfollow"
+            elif action == "Unfollow":
+                request.user.following.remove(user_profile)
+                context["action"] = "Follow"
+        return render(request, "network/profile.html", context)
+    else:
+        if request.user in user_profile.followers.all():
+            context["action"] = "Unfollow"
+        else:
+            context["action"] = "Follow"
+
+        return render(request, "network/profile.html", context)
